09e5a281cc051bbb138bdc512a9a05ac7a9cc01e
Miguel Prada <mapra99@gmail.com>
2019-10-07 15:50:04 -0500

    Added regex to validate the format of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     validates :name,    presence: true,
                         length: {maximum: 50}
+    
     validates :email,   presence: true,
-                        length: {maximum: 255}
+                        length: {maximum: 255},
+                        format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 end
