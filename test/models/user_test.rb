72813f3efa0c0c0a70317c1c944a0e8ccc255b74
Miguel Prada <mapra99@gmail.com>
2019-10-07 15:43:31 -0500

    added tests for validation of emails

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,30 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 256
     assert_not @user.valid?
   end
+
+  test "emails that should be valid" do
+    valid_addresses = %w[user@example.com
+                        USER@foo.COM
+                        A_US-ER@foo.bar.org
+                        first.last@foo.jp
+                        alice+bob@baz.cn]
+    
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"
+    end
+  end
+
+  test "emails that shouldn't be valid" do
+    invalid_addresses = %w[user@example,com
+                            user_at_foo.org 
+                            user.name@example.
+                            foo@bar_baz.com 
+                            foo@bar+baz.com]
+
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} shouldn't be valid"
+    end                   
+  end
 end
