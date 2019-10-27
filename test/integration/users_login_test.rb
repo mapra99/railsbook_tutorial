acc474db0383c95bf5352add87d0a43638294afb
Miguel Prada <mapra99@gmail.com>
2019-10-27 03:53:28 -0500

    tested successful login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test 'danger flash message should appear only once' do
     get login_path
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email: '', password: '' } }
     assert_not flash.empty?
     get root_path
     assert flash.empty?
@@ -14,10 +20,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     user.save
 
     get login_path
-    post login_path, params: { session: { email: "this@example.com", password: "password123" } }    
+    post login_path, params: { session: { email: 'this@example.com', password: 'password123' } }
     follow_redirect!
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test 'login with valid information' do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password123' } }
+
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
   end
 end
