78e128c0a9750a4ac3a5a0efa5b15db152e67b62
Miguel Prada <mapra99@gmail.com>
2019-10-28 09:18:30 -0500

    added method to check that user is current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -41,5 +44,18 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email,
                                  :password,
                                  :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
