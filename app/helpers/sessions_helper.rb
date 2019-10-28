1ed9b1dabd1e4038f94d73740ee4454dcd4a7c7d
Miguel Prada <mapra99@gmail.com>
2019-10-27 20:10:55 -0500

    fixed subtle bugs

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,7 +29,14 @@ module SessionsHelper
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,12 @@ class User < ApplicationRecord
 
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
