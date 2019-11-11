e2489c8710785ac7611a994466931da219f68d49
Miguel Prada <mapra99@gmail.com>
2019-11-11 16:28:10 -0500

    i'm back baby

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,10 +47,10 @@ class UsersController < ApplicationController
   end
 
   def logged_in_user
-    unless logged_in?
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
-    end
+    return if logged_in?
+
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
   end
 
   # Confirms the correct user.
