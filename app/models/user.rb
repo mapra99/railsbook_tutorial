66ce09bc767798ca684a5e1dcc5eb6f8b8e5c647
Miguel Prada <mapra99@gmail.com>
2019-11-12 15:40:29 -0500

    move the email delivery and user activation to new instance methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,6 +45,17 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  
+  private
+
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
