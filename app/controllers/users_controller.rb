c1bb852fab8917cf819e8fa817bde2262c3259a0
Miguel Prada <mapra99@gmail.com>
2019-11-12 15:35:24 -0500

    only activated users can be listed in users/index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 15)
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 15)
   end
 
   def destroy 
