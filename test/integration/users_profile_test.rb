5de06359b163a760df6bfd5ef9fc373eac4cd354
Miguel Prada <mapra99@gmail.com>
2019-11-13 17:58:22 -0500

    added test for profile display

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,7 +1,20 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UrsersProfileTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include ApplicationHelper
+
+  def setup
+    @user = users(:miguel)
+  end
+
+  test 'profile display' do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'h1', text: @user.name
+    assert_select 'h1>img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+  end
 end
