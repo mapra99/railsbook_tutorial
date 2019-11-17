f721de48fd1eac0fc73a88ac8ae81b980e2f1ca7
Miguel Prada <mapra99@gmail.com>
2019-11-16 21:22:33 -0500

    added view and controller for microopst destroy action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -10,15 +11,22 @@ class MicropostsController < ApplicationController
       @feed_items = []
       render 'static_pages/home'
     end
-    
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = 'Micropost Deleted'
+    redirect_to request.referrer || root_path
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_path unless @micropost
   end
 end
