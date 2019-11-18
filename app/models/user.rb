d45d4787f053e565794769888c492fe05bc6da95
Miguel Prada <mapra99@gmail.com>
2019-11-18 15:22:27 -0500

    added helper methods for following actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
 
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
 
   before_save :downcase_email
   before_create :create_activation_digest
@@ -74,6 +76,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where(user_id: id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
